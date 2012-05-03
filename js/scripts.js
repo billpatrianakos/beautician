837932f208a80946c0909fa49bfe55d390f9e7da
Bill Patrianakos <bill@chooseclever.com>
2012-05-03 11:51:22 -0500

    Adding a Markdown guide

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-/*
- * Put all your JavaScript scripts here. Plugins go elsewhere.
- */
+// Markdown guide
+$(document).ready(function() {
+	$(".md-guide").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'elastic',
+		closeEffect	: 'none'
+	});
+});
